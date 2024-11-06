15842e94b76679171fba883d905a4d6ad86b05c9
Ashraf <mm0019701>
2024-11-06 14:16:48 +0200

    Implemented AccountController to allow users to manage their accounts

---
+++ ELearning.Api/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using ELearning.Data.Abstractions.Extensions;
+using ELearning.Data.Abstractions.ResultPattern;
+using ELearning.Data.Contracts.Users;
+using ELearning.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELearning.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(IUserService userService) : ControllerBase
+{
+    // await _userManager.Users it like await _context.Users
+    //it's better to use _context then _userManger
+
+    private readonly IUserService _userService = userService;
+
+    [HttpGet("")]
+    public async Task<IActionResult> Info()
+    {
+        var result = await _userService.GetProfileAsync(User.GetUserId()!);
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+
+    [HttpPut("info")]
+    public async Task<IActionResult> Info([FromBody] UpdateProfileRequest request, CancellationToken cancellationToken)
+    {
+        var result = await _userService.UpdateProfileAsync(User.GetUserId()!, request);
+        return result.IsSuccess ? NoContent() : result.ToProblem();
+    }
+
+    [HttpPut("change-password")]
+    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest request)
+    {
+        var result = await _userService.ChangePasswordAsync(User.GetUserId()!, request);
+        return result.IsSuccess ? NoContent() : result.ToProblem();
+    }
+}
